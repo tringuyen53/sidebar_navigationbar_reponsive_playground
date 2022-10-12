1fd1c5424152e9bbf155a723bfc75a0c4e37c265
tringuyen53 <nguyenduongtri0503@gmail.com>
2022-10-12 09:34:58 +0700

    change icon menu color

--- lib/responsive/mobile_body.dart
+++ lib/responsive/mobile_body.dart
@@ -22,17 +22,23 @@ class MyMobileBody extends ConsumerWidget {
     return Scaffold(
       key: scaffoldKey,
       appBar: AppBar(
+        backgroundColor: Colors.white,
         leading: IconButton(
-          onPressed: selectedIndex != 0 ? null : () {
-            scaffoldKey.currentState?.openDrawer();
-          },
-          icon: const Icon(Icons.menu),
+          onPressed: selectedIndex != 0
+              ? null
+              : () {
+                  scaffoldKey.currentState?.openDrawer();
+                },
+          icon: Icon(
+            Icons.menu,
+            color: selectedIndex != 0 ? Colors.grey : Colors.black,
+          ),
         ),
       ),
       drawer: Drawer(
         child: Scaffold(
           appBar: AppBar(
-            backgroundColor: Colors.blueGrey,
+            backgroundColor: Colors.white,
             title: const Text(
               'Region',
               style: TextStyle(fontSize: 20),
@@ -41,7 +47,10 @@ class MyMobileBody extends ConsumerWidget {
               onPressed: () {
                 Navigator.pop(context);
               },
-              icon: const Icon(Icons.menu_open),
+              icon: const Icon(
+                Icons.menu_open,
+                color: Colors.black,
+              ),
             ),
           ),
           body: const ListGroupView(),
